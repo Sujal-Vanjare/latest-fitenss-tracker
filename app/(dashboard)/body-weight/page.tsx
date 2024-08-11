c18a53e9fb2f6ce9d46e762193493ac1ed6c02b6
Sujal Vanjare <141014768+Sujal-Vanjare@users.noreply.github.com>
2024-08-12 00:40:09 +0530

    Body weight chart corrected

--- app/(dashboard)/body-weight/page.tsx
+++ app/(dashboard)/body-weight/page.tsx
@@ -1,3 +1,9 @@
+import BodyWeightChart from "@/components/charts/body-weight-chart";
+
 export default function Page() {
-  return <div>body weight</div>;
+  return (
+    <div className="p-4">
+      <BodyWeightChart />
+    </div>
+  );
 }

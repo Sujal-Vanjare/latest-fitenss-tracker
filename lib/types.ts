64f0beb96226160ee20d605da3ffad88c8f216e0
Sujal Vanjare <141014768+Sujal-Vanjare@users.noreply.github.com>
2024-08-16 21:21:25 +0530

    demo data removed and types fixed

--- lib/types.ts
+++ lib/types.ts
@@ -1,16 +1,10 @@
-type ChartEntry = {
-  recorded_at: string;
-  body_weight: number;
-  bodyweight_change: number;
-};
-
 type MonthlyData = {
   month: string;
   weight: number;
 };
 
 type BodyWeightEntry = {
-  id: number; // or string, depending on your database setup
+  id: number;
   body_weight: number;
   recorded_at: string;
 };

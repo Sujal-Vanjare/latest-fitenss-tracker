ab84dca332f9fb12e446d2cf5c0da34d081f4f75
Sujal Vanjare <141014768+Sujal-Vanjare@users.noreply.github.com>
2024-08-17 20:49:43 +0530

    mobile dashboard fixed

--- components/dashboard.tsx
+++ components/dashboard.tsx
@@ -145,22 +145,27 @@ export function Dashboard({
               </SheetDescription>
               <nav className="grid gap-2 text-lg font-medium">
                 <Link
-                  href="#"
-                  className="flex items-center gap-2 text-lg font-semibold"
+                  href="/"
+                  className="flex items-center gap-4 mx-[-0.65rem] px-3 py-4"
                 >
                   <Package2 className="h-6 w-6" />
-                  <span className="sr-only">Fitness Tracker</span>
+                  <span className="text-xl font-bold">Fitness Tracker</span>
                 </Link>
                 <Link
                   href="/body-weight"
-                  className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
+                  className={cn(
+                    "mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground",
+                    pathname === "/body-weight"
+                      ? "bg-muted text-foreground"
+                      : ""
+                  )}
                 >
                   <Home className="h-5 w-5" />
                   Body Weight
                 </Link>
                 <Link
                   href="#"
-                  className="mx-[-0.65rem] flex items-center gap-4 rounded-xl bg-muted px-3 py-2 text-foreground hover:text-foreground"
+                  className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
                 >
                   <ShoppingCart className="h-5 w-5" />
                   Push Day

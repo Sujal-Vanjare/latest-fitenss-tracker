4fdf1e1ddde309f2b59f3897b6e20b78ee75617c
Sujal Vanjare <141014768+Sujal-Vanjare@users.noreply.github.com>
2024-08-24 15:35:27 +0530

    dashboard overflow fixes

--- components/dashboard.tsx
+++ components/dashboard.tsx
@@ -1,14 +1,7 @@
 "use client";
 import Link from "next/link";
 import { LiaWeightSolid } from "react-icons/lia";
-import {
-  Bell,
-  Dumbbell,
-  LineChart,
-  Menu,
-  Package2,
-  Search,
-} from "lucide-react";
+import { Bell, Dumbbell, Menu, Package2, Search } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -96,18 +89,18 @@ export function Dashboard({
   return (
     <div className="fixed grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
       <div className="hidden border-r bg-muted/40 md:block">
-        <div className="flex h-full max-h-screen flex-col gap-2">
+        <div className="flex h-screen flex-col gap-2">
           <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
             <Link href="/" className="flex items-center gap-2 font-semibold">
               <Package2 className="h-6 w-6" />
-              <span className="">Fitness Tracker</span>
+              <span>Fitness Tracker</span>
             </Link>
             <Button variant="outline" size="icon" className="ml-auto h-8 w-8">
               <Bell className="h-4 w-4" />
               <span className="sr-only">Toggle notifications</span>
             </Button>
           </div>
-          <div className="flex-1">
+          <div>
             <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
               <Link
                 href="/body-weight"
@@ -254,7 +247,7 @@ export function Dashboard({
                   className="flex items-center gap-2 font-semibold"
                 >
                   <Package2 className="h-6 w-6" />
-                  <span className="">Fitness Tracker</span>
+                  <span>Fitness Tracker</span>
                 </Link>
                 <Button
                   variant="outline"
@@ -265,7 +258,7 @@ export function Dashboard({
                   <span className="sr-only">Toggle notifications</span>
                 </Button>
               </div>
-              <div className="flex-1">
+              <div>
                 <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
                   <Link
                     href="/body-weight"

4c45cbb1031194e595a3291e3e900f1ac32f64f4
Sujal Vanjare <141014768+Sujal-Vanjare@users.noreply.github.com>
2024-08-25 16:18:06 +0530

    useSearchParams() suspense error solved

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,10 +1,13 @@
+"use client";
 import SignIn from "@/components/auth/signin";
-import React from "react";
+import { Suspense } from "react";
 
 export default function page() {
-	return (
-		<div className="flex items-center justify-center h-screen">
-			<SignIn />
-		</div>
-	);
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <Suspense fallback={<div>Loading...</div>}>
+        <SignIn />
+      </Suspense>
+    </div>
+  );
 }

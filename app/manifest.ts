434fe94ef57a9ae8137ea67ef038374179c82c87
Sujal Vanjare <141014768+Sujal-Vanjare@users.noreply.github.com>
2024-08-30 23:51:17 +0530

    favicon and manifest file added

---
+++ app/manifest.ts
@@ -0,0 +1,39 @@
+import type { MetadataRoute } from "next";
+import siteMetadata from "@/lib/siteMetaData";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: siteMetadata.title,
+    short_name: siteMetadata.title,
+    description: siteMetadata.description,
+    start_url: "/",
+    display_override: ["fullscreen", "minimal-ui"],
+    display: "standalone",
+
+    background_color: "#000000",
+    theme_color: "#000000",
+    icons: [
+      {
+        src: "/favicon-16x16.png",
+        sizes: "16x16",
+        type: "image/png",
+      },
+      {
+        src: "/favicon-32x32.png",
+        sizes: "32x32",
+        type: "image/png",
+      },
+
+      {
+        src: "/favicon-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/favicon-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  };
+}

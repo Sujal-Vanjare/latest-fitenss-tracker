03295450750c0502018e733a4fcdbb935c3edd3e
Sujal Vanjare <141014768+Sujal-Vanjare@users.noreply.github.com>
2024-12-04 23:58:52 +0530

    added og image url

--- config/site.js
+++ config/site.js
@@ -2,7 +2,8 @@ export const siteConfig = {
   shortName: "Track Workout",
   name: "Track all Gym exercises",
   url: "https://trackworkout.vercel.app",
-  ogImage: "",
+  ogImage:
+    "https://res.cloudinary.com/drzcgtzx8/image/upload/v1733225356/portfolio/fitness-tracker/uwby6ij5zx8n8mrlutnb.png",
   description:
     "Track Workout is a top-tier fitness tracker designed for push-pull-legs routines. It offers comprehensive body weight tracking and visually stunning charts to help you monitor and enhance your fitness journey with ease.",
   links: {
